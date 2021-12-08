0f221fa8cefe95e201d1e14f4038427373c087d5
jorgezerpa <jorgezerpacoder@gmail.com>
2021-12-08 11:08:01 -0400

    style correction, add icons, improve modal function for new images

--- main.js
+++ main.js
@@ -4,25 +4,22 @@ let files= document.getElementById("file");
 let image; //all images showed;
 let modal= document.querySelector(".img-modal");
 let hide_modal= document.querySelector(".hide-modal");
-console.log(hide_modal);
+
+
+            //xample images
+            loadImage("images/bailey-zindel-NRQV-hBF10M-unsplash.jpg");
+            loadImage("images/boxed-water-is-better-jbcY_yvsDzk-unsplash.jpg");
+            loadImage("images/boxed-water-is-better-rXJXsecq8YU-unsplash.jpg");
+            loadImage("images/pietro-de-grandi-T7K4aEPoGGk-unsplash.jpg");
+            loadImage("images/pine-watt-2Hzmz15wGik-unsplash.jpg");
+            
 
             //show new images
 files.addEventListener("change", ()=>{
     for(let i=0; i<files.files.length; i++){
-        newImage(files.files[i])
+        processImage(files.files[i])
     }
 })
-
-
-
-            //xample images
-loadImage("images/bailey-zindel-NRQV-hBF10M-unsplash.jpg");
-loadImage("images/boxed-water-is-better-jbcY_yvsDzk-unsplash.jpg");
-loadImage("images/boxed-water-is-better-rXJXsecq8YU-unsplash.jpg");
-loadImage("images/pietro-de-grandi-T7K4aEPoGGk-unsplash.jpg");
-loadImage("images/pine-watt-2Hzmz15wGik-unsplash.jpg");
-
-
 
                 //modal
 image= document.querySelectorAll(".img img");
@@ -39,20 +36,31 @@ hide_modal.addEventListener("click", ()=>{
 })
 
 
-            //show images
-function loadImage(img_url){
-    images.innerHTML+=`
-    <div class="img">
-        <img src=${img_url}>
-    </div>
-    `
-}
 
-            //show a new image(s) from the input file
-function newImage(image){
+            //Process a new image(s) from the input file
+function processImage(image){
     let reader= new FileReader();
     reader.readAsDataURL(image);
     reader.onload= function(){
-    loadImage(this.result);
+    loadImage(this.result);                                                                                                                                             
     }
 }
+
+
+            //show images
+function loadImage(img_url){
+    images.innerHTML+=`
+            <div class="img">
+                <img src=${img_url}>
+            </div>
+            `
+
+    image= document.querySelectorAll(".img img");
+        for(let i=0; i<image.length; i++){
+          image[i].addEventListener("click",(event)=>{
+            modal.style.backgroundImage="url("+ event.currentTarget.src+")";
+            modal.style.visibility="visible";
+           })
+        }
+    }
+            
