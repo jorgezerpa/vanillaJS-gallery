2d7d1eabccf9837aac4960fc186815a3b1ff2732
jorgezerpa <jorgezerpacoder@gmail.com>
2021-12-07 22:51:10 -0400

    second commit

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
 'use strict';
 let images= document.querySelector(".images");
 let files= document.getElementById("file");
+let image; //all images showed;
+let modal= document.querySelector(".img-modal");
+let hide_modal= document.querySelector(".hide-modal");
+console.log(hide_modal);
 
-let img_store=[];
-
-
-files.addEventListener("change", (file)=>{
-
+            //show new images
+files.addEventListener("change", ()=>{
     for(let i=0; i<files.files.length; i++){
-    let reader= new FileReader();
-        reader.readAsDataURL(files.files[i]);
-        reader.onload= function(){
-            loadImage(this.result);
-            img_store.push(this.result);
-        }
+        newImage(files.files[i])
     }
-    console.log(img_store);
-
 })
+
 
 
             //xample images
@@ -28,10 +23,36 @@ loadImage("images/pietro-de-grandi-T7K4aEPoGGk-unsplash.jpg");
 loadImage("images/pine-watt-2Hzmz15wGik-unsplash.jpg");
 
 
+
+                //modal
+image= document.querySelectorAll(".img img");
+
+for(let i=0; i<5; i++){
+    image[i].addEventListener("click",(event)=>{
+        modal.style.backgroundImage="url("+ event.currentTarget.src+")";
+        modal.style.visibility="visible";
+    })
+}
+
+hide_modal.addEventListener("click", ()=>{
+    modal.style.visibility="hidden";
+})
+
+
+            //show images
 function loadImage(img_url){
     images.innerHTML+=`
     <div class="img">
         <img src=${img_url}>
     </div>
     `
+}
+
+            //show a new image(s) from the input file
+function newImage(image){
+    let reader= new FileReader();
+    reader.readAsDataURL(image);
+    reader.onload= function(){
+    loadImage(this.result);
+    }
 }
